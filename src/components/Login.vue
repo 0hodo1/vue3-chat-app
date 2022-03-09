450ca8a3cae3e6bc81c0ea0996b0cf12161ffc1b
Ömer Hodo <omerhodo@gmail.com>
2022-03-09 14:40:36 +0300

    login process succedd

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,25 +1,34 @@
 <template>
-  <form @submit.prevent="register">
+  <form @submit.prevent="signin">
     <h4>Login Page</h4>
     <input type="email" placeholder="please a email" v-model="email" />
     <input type="password" placeholder="please a password" v-model="password" />
     <button>Login</button>
   </form>
+  <div class="error">
+    {{ hata }}
+  </div>
 </template>
 
 <script>
 import { ref } from "vue";
+import useLogin from "../composables/useLogin";
+
 export default {
   setup() {
     const email = ref("");
     const password = ref("");
-    const register = () => {
-      console.log(email.value, password.value);
+
+    const { hata, login } = useLogin();
+
+    const signin = async () => {
+      login(email.value, password.value);
     };
     return {
       email,
       password,
-      register,
+      signin,
+      hata,
     };
   },
 };
